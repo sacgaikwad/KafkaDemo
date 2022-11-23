2149a88e66fbe0928eac7d960ed52dc3ff4d93ed
unknown <sachingaikwad2109@gmail.com>
2022-11-23 11:34:43 +0530

    added docker file

---
+++ Services/KafkaConsumer.cs
@@ -0,0 +1,23 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KafkaDemo.Services
+{
+    public class KafkaConsumer
+    {
+        private string _topicName;
+        private ConsumerConfig _consumerConfig;
+        private IConsumer<string, string> _consumer;
+        private static readonly Random rand = new Random();
+
+        public KafkaConsumer(ConsumerConfig config, string topicName)
+        {
+            this._topicName = topicName;
+            this._consumerConfig = config;            
+            this._consumer.Subscribe(topicName);
+        }
+    }
+}
